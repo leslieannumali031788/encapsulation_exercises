4cb3c392308c1f8f462c1489132aaa41c8bebdb3
leslieannumali031788 <umalileslieann10@gmail.com>
2026-06-05 00:17:53 +0800

    Instantiation

--- Car/car.py
+++ Car/car.py
@@ -2,4 +2,5 @@ from car_run import Car
 if __name__ == '__main__':
     car_year = input("Enter car year's model:")
     car_make = input("Enter car make's name:")
-    
+
+    my_car = Car(car_year, car_make)
