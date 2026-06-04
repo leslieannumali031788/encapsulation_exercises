6068621aa2f9970e2b71e9876b9ec2b48f2e241f
leslieannumali031788 <umalileslieann10@gmail.com>
2026-06-04 23:47:24 +0800

    Speedometer started to read at zero km/h.

--- Car/car_run.py
+++ Car/car_run.py
@@ -2,6 +2,7 @@ class Car:
     def __init__(self, year_model, make):
         self.year_model = year_model
         self.make = make
+        self.__current_speed = 0
 
     def accelerate(self):
         pass
