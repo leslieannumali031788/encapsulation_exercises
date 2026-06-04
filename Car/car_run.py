198f90d8226855742f88f1bd6f645220ce958a7d
leslieannumali031788 <umalileslieann10@gmail.com>
2026-06-04 23:45:29 +0800

    Assigned year model.

--- Car/car_run.py
+++ Car/car_run.py
@@ -1,10 +1,9 @@
 class Car:
     def __init__(self, year_model, make):
-        pass
+        self.year_model = year_model
 
     def accelerate(self):
         pass
 
     def brake(self):
         pass
-    
